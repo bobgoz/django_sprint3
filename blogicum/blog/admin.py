697592bccf5c3e735e0a07661007b58fc9410acb
Никита Зайцев <nikita.zaytsev2013@gmail.com>
2025-01-19 00:28:20 +0300

    Готово

---
+++ blogicum/blog/admin.py
@@ -0,0 +1,69 @@
+from django.contrib import admin
+
+from .models import Category, Location, Post
+
+
+# @admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'slug',
+        'created_at',
+        'is_published',
+    )
+    list_editable = (
+        'is_published',
+
+    )
+    search_fields = ('title',)
+    list_filter = (
+        'title',
+        'slug',
+        'created_at',
+        'is_published',
+        )
+    list_display_links = ('title',)
+
+# @admin.register(Location)
+class LocationAdmin(admin.ModelAdmin):
+    list_display = (
+    'name',
+    'created_at',
+    'is_published',
+    )
+    list_editable = (
+        'is_published',
+    )
+    list_filter = (
+        'name',
+        'created_at',
+        'is_published',
+        )
+    list_display_links = ('name',)
+
+# @admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'text',
+        'category',
+        'is_published',
+    )
+    list_editable = (
+        'is_published',
+
+    )
+    search_fields = ('title',)
+    list_filter = (
+        'category',
+        'is_published',
+)
+    list_display_links = ('title',)
+
+admin.site.register(Post, PostAdmin) 
+admin.site.register(Location, LocationAdmin) 
+admin.site.register(Category, CategoryAdmin)
+admin.site.empty_value_display = 'Не задано'
+
+
+
---
+++ blogicum/blogicum/urls.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from django.urls import path
+from django.urls import include
+from django.conf import settings
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('blog.urls')),
+    path('pages/', include('pages.urls')),
+]
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns += (path('__debug__/', include(debug_toolbar.urls)),) 
